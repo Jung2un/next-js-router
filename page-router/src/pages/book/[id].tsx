98d3456acc65a8bbd1bdf5ee7a888831ea125638
Jung2un <a_su@daum.net>
2025-05-27 23:33:16 +0900

    feat: 메인 페이지 ISR 처리 및 SEO 설정 추가

--- page-router/src/pages/book/[id].tsx
+++ page-router/src/pages/book/[id].tsx
@@ -1,9 +1,21 @@
-import style from './[id].module.css';
+import Head from "next/head";
+import { useRouter } from "next/router";
 import fetchOneBook from "@/lib/fetch-one-book";
-import {GetServerSidePropsContext, InferGetServerSidePropsType} from "next";
+import style from './[id].module.css';
+import {GetStaticPropsContext, InferGetStaticPropsType} from "next";
 
+export const getStaticPaths = () => {
+  return {
+    paths: [
+      { params: { id: "1"} },
+      { params: { id: "2"} },
+      { params: { id: "3"} },
+    ],
+    fallback: true,
+  };
+};
 
-export const getServerSideProps = async (context: GetServerSidePropsContext) => {
+export const getStaticProps = async (context: GetStaticPropsContext) => {
   const id = context.params!.id;
   const book = await fetchOneBook(Number(id));
 
@@ -14,8 +26,23 @@ export const getServerSideProps = async (context: GetServerSidePropsContext) => 
   }
 }
 
-export default function Page({book}: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  if(!book) return "문제가 발상했습니다 다시 시도하세요";
+export default function Page({book}: InferGetStaticPropsType<typeof getStaticProps>) {
+  const router = useRouter();
+  if (router.isFallback) {
+    return (
+      <>
+        <Head>
+          <title>목록</title>
+          <meta property="og:image" content="/thumbnail.png" />
+          <meta property="og:title" content="목록" />
+          <meta property="og:description" content="등록된 도서를 만나보세요" />
+        </Head>
+        <div>로딩중입니다</div>
+      </>
+    );
+  }
+  if (!book) return "문제가 발생했습니다 다시 시도하세요";
+
   const {
     id,
     title,
@@ -27,17 +54,25 @@ export default function Page({book}: InferGetServerSidePropsType<typeof getServe
   } = book;
 
   return (
-    <div className={style.container}>
-      <div className={style.img_container} style={{backgroundImage: `url('${coverImgUrl}')`}}>
-        <img src={coverImgUrl}/>
-      </div>
-      <div className={style.title}>{title}</div>
-      <div className={style.subTitle}>{subTitle}</div>
-      <div className={style.author}>
-        <div>{author} | {publisher}</div>
-      </div>
-      <div className={style.description}>{description}</div>
+    <>
+      <Head>
+        <title>{title} 상세결과</title>
+        <meta property="og:image" content={coverImgUrl} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+      </Head>
+      <div className={style.container}>
+        <div className={style.img_container} style={{backgroundImage: `url('${coverImgUrl}')`}}>
+          <img src={coverImgUrl}/>
+        </div>
+        <div className={style.title}>{title}</div>
+        <div className={style.subTitle}>{subTitle}</div>
+        <div className={style.author}>
+          <div>{author} | {publisher}</div>
+        </div>
+        <div className={style.description}>{description}</div>
 
-    </div>
+      </div>
+    </>
   );
 }
--- page-router/src/pages/search/index.tsx
+++ page-router/src/pages/search/index.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react";
+import Head from "next/head";
 import fetchBooks from "@/lib/fetch-books";
 import BookItem from "@/components/book-item";
 import SearchLayout from "@/components/search-layout";
@@ -19,11 +20,19 @@ export const getServerSideProps = async (
 
 export default function Page({books}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
-    <div>
-      {books.map((item) => (
-        <BookItem key={item.id} {...item} />
-      ))}
-    </div>
+    <>
+      <Head>
+        <title>목록 - 검색결과</title>
+        <meta property="og:image" content="/thumbnail.png" />
+        <meta property="og:title" content="목록 - 검색결과" />
+        <meta property="og:description" content="등록된 도서를 만나보세요" />
+      </Head>
+      <div>
+        {books.map((item) => (
+          <BookItem key={item.id} {...item} />
+        ))}
+      </div>
+    </>
   );
 }
 
