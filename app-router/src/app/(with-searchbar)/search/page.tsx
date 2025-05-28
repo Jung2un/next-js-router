7a5a8a07856545826c1003ebe934fd5551b9338a
Jung2un <a_su@daum.net>
2025-05-28 16:47:40 +0900

    feat: 페이지 라우팅, 레이아웃 구성 및 API 데이터 패칭 로직 구현

---
+++ app-router/src/app/(with-searchbar)/search/page.tsx
@@ -0,0 +1,26 @@
+import {BookData} from "@/types";
+import BookItem from "@/components/book-item";
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{ q?: string }>;
+}) {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${(await searchParams).q}`,
+    {cache: "force-cache"}
+  )
+  if (!res.ok) {
+    return <div>오류가 발생했습니다</div>
+  }
+
+  const books: BookData[] = await res.json();
+
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+}
